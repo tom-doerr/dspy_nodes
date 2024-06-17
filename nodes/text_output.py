16e265ceaf8b1570ef64631161e9890e611d9eb8
Tom Dörr <tomdoerr96@gmail.com>
2024-06-17 23:55:37 +0200

    Add file

--- nodes/text_output.py
+++ nodes/text_output.py
@@ -8,10 +8,18 @@ class TextOutput:
                 }
 
     RETURN_TYPES = ("STRING",)
+    # RETURN_TYPES = ()
     FUNCTION = "text_output"
     OUTPUT_NODE = True
     CATEGORY = "DSPy"
+    INPUT_IS_LIST = True
+    OUTPUT_IS_LIST = (True,)
 
-    def text_output(self, text):
-        print(text)
-        return (text,)
+    def text_output(self, text='test123'):
+        # print(text)
+        text_joined = ''.join(text)
+        text = text_joined
+        print(f'text output input text: {text}')
+        # return (text,)
+        # return {"ui": {"text": (text,)}}
+        return {"ui": {"text": text}, "result": (text,)}
