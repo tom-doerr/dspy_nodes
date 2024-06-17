275ae8e993dc66d50013da7015e3b7e0c0128be8
Tom Dörr <tomdoerr96@gmail.com>
2024-06-17 23:54:58 +0200

    Add file

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from .nodes.hello_world import *
+from .nodes.predict import *
+from .nodes.text_field import *
+from .nodes.text_output import *
+from .nodes.show_text import *
+
+
+NODE_CLASS_MAPPINGS = {
+    "Print Hello World": PrintHelloWorld,
+    "Predict": Predict,
+    "Text Field": TextField,
+    "Text Output": TextOutput,
+    "Show Text": ShowText,
+    }
+    
+print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")
