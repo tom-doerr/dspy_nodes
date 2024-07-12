cbb427007b74c9b8d6e548730e04c34749e040b7
Tom Dörr <tomdoerr96@gmail.com>
2024-07-12 03:37:03 +0200

    Add option to set max tokens

--- nodes/model.py
+++ nodes/model.py
@@ -8,6 +8,7 @@ class Model:
                
         return {"required": {       
                     "model": ("STRING", {"default": "microsoft/Phi-3-medium-128k-instruct"}),
+                    "max_tokens": ("INT", {"default": 100, "min": 1}),
                     }
                 }
 
@@ -18,11 +19,12 @@ class Model:
     OUTPUT_NODE = True
     CATEGORY = "DSPy"
 
-    def set_params(self, model):
+    # def set_params(self, model):
+    def set_params(self, model, max_tokens):
         server_settings['model'] = model
         print("====== model file server_settings:", server_settings)
         # lm = dspy.HFClientVLLM(model=server_settings['model'], port=38242, url="http://localhost", max_tokens=200)
         # lm = dspy.HFClientVLLM(model=model, port=38242, url="http://localhost", max_tokens=200)
-        lm = dspy.OpenAI(model=model, api_base="http://localhost:38242/v1/", api_key="EMPTY")
+        lm = dspy.OpenAI(model=model, api_base="http://localhost:38242/v1/", api_key="EMPTY", max_tokens=max_tokens)
         # return 'text'
         return [lm]
